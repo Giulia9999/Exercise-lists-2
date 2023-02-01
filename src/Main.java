b45d61a5b55f60922be5f677cd03f4d837603fba
Giulia <giuliac35@gmail.com>
2023-02-01 13:24:33 +0100

    Print sorted randomNumbers list

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args){
@@ -15,3 +16,4 @@ public class Main {
         System.out.println(randomNumbers);
     }
 }
+
