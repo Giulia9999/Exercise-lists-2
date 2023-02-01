3732bb23103dcb59c278c3f8acf2d327791a413c
Giulia <giuliac35@gmail.com>
2023-02-01 12:56:02 +0100

    Add divisorsOf50 values to randomaNumbers list. Print randomNumbers

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args){
         List<Integer> divisorsOf52 = new ArrayList<>(Arrays.asList(1, 2, 4, 13, 26, 52));
         List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(1, 60, 13, 12));
+        randomNumbers.addAll(divisorsOf52);
+        System.out.println(randomNumbers);
     }
 }
