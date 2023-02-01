748f336f786632ccd4516ac1d4195d8eeaa14def
Giulia <giuliac35@gmail.com>
2023-02-01 12:59:25 +0100

    Create set with randomNumbers values

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         List<Integer> divisorsOf52 = new ArrayList<>(Arrays.asList(1, 2, 4, 13, 26, 52));
         List<Integer> randomNumbers = new ArrayList<>(Arrays.asList(1, 60, 13, 12));
         randomNumbers.addAll(divisorsOf52);
-        System.out.println(randomNumbers);
+        System.out.println(randomNumbers + " - size: " + randomNumbers.size());
+        Set<Integer> numberSet = new HashSet<>(randomNumbers);
     }
 }
